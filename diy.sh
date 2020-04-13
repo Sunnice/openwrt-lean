4ba9ea6d45ffca2fb2afa2ede9b57e061b040929
Sunnice <44739306+Sunnice@users.noreply.github.com>
2020-04-13 20:17:49 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,7 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR.git package/koolproxyR
+git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/koolproxyR
 git clone https://github.com/pymumu/smartdns.git package/smartdns
 git clone https://github.com/ujincn/luci-app-smartdns-compat.git package/luci-app-smartdns
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
